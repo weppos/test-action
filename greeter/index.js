b06e2dd941683a73e6831492fe56f25e86bb4eb3
Simone Carletti <weppos@weppos.net>
2020-03-21 16:12:49 +0100

    Terminate statement

--- greeter/index.js
+++ greeter/index.js
@@ -8,7 +8,7 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
