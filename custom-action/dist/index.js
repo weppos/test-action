301f43bdae0d71efd861cb15b06ad4990d75bc7b
Simone Carletti <weppos@weppos.net>
2020-03-21 18:54:41 +0100

    Repo not Pulls

--- custom-action/dist/index.js
+++ custom-action/dist/index.js
@@ -498,7 +498,7 @@ async function run() {
     const repoToken = core.getInput('repo-token');
     const octokit = new github.GitHub(repoToken);
 
-    const repo = await octokit.pulls.get({
+    const repo = await octokit.repos.get({
       owner: 'weppos',
       repo: 'test-action',
     });
